059d74b0e914a95e8b4dd922de5534de7e6d8a03
Michelle M. Muñoz-Dorna <michelle@m3orange.com>
2026-05-26 15:56:26 -0400

    Root set to Hostinger again
    It all broke, so removed mm.info

--- config.php
+++ config.php
@@ -13,8 +13,8 @@ if ($isLocal) {
     define('ROOT_PATH', __DIR__ . '/');
 } else {
     // Production server    
-    // define('BASE_URL', 'https://lightpink-hedgehog-479399.hostingersite.com/');
-    define('BASE_URL', 'https://www.michellemunoz.info/');
+    define('BASE_URL', 'https://lightpink-hedgehog-479399.hostingersite.com/');
+    // define('BASE_URL', 'https://www.michellemunoz.info/');
     define('ROOT_PATH', __DIR__ . '/');
 }
 
