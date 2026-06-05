65b365ca7f6c3353edd4d6cd128e3f6af329dd7a
Michelle M. Muñoz-Dorna <michelle@m3orange.com>
2026-06-05 18:19:47 -0400

    Adding links in sidenav

--- includes/global-nav.php
+++ includes/global-nav.php
@@ -21,10 +21,7 @@
                         </button>
                         <div class="navbar-collapse collapse justify-content-end" id="navbar-collapse-toggle-1">
                             <ul id="accordion" class="nav navbar-nav no-margin alt-font text-normal" data-in="fadeIn" data-out="fadeOut">
-
-
-                                <li><a href="<?= BASE_URL ?>index.php" title="Home">Home</a></li>
-                                    <li class="nav-divider"></li>
+                                <li><a href="<?= BASE_URL ?>index.php" title="Home">Home</a></li><li class="nav-divider"></li>
                                 <li><a href="<?= BASE_URL ?>projects/usic/index.php" title="USIC">USIC</a></li>
                                 <li><a href="<?= BASE_URL ?>projects/citybox/index.php" title="Atlas UI">CityBox UI</a></li>
                                 <li><a href="<?= BASE_URL ?>projects/admin/index.php" title="Admin">Admin</a></li>
@@ -46,33 +43,30 @@
                     </div>
                 </div>
             </nav>
+
+
+
             <!-- end navigation --> 
             <div class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-right" id="cbp-spmenu-s2">
                 <button class="close-button-menu side-menu-close" id="close-pushmenu"></button>
                 <div class="d-table padding-twelve-all height-100 width-100 text-center">
                     <div class="d-table-cell align-top padding-70px-top position-relative">
                         <div class="row">
-                            <div class="col-lg-12 margin-70px-bottom">
-                                <img src="images/logo-black-big.png"/>
-                            </div>
-                            <div class="col-lg-12 margin-70px-bottom">
-                                <img src="images/sidebar-image1.png"/>
-                            </div>
+
                             <div class="col-lg-12">
-                                <h5 class="alt-font text-extra-dark-gray"><span class="d-block font-weight-300 text-dark-gray">The world's most</span><strong>powerful website builder.</strong></h5>
+                                <ul id="accordion" class="nav navbar-nav no-margin alt-font text-normal" data-in="fadeIn" data-out="fadeOut">
+                                    <li><a href="<?= BASE_URL ?>index.php" title="Home">Home</a></li>
+                                        <li class="nav-divider"></li>
+                                    <li><a href="<?= BASE_URL ?>projects/usic/index.php" title="USIC">USIC</a></li>
+                                    <li><a href="<?= BASE_URL ?>projects/citybox/index.php" title="Atlas UI">CityBox UI</a></li>
+                                    <li><a href="<?= BASE_URL ?>projects/admin/index.php" title="Admin">Admin</a></li>
+                                    <li><a href="<?= BASE_URL ?>projects/atlas-design-system/index.php" title="Atlas Design System">Atlas</a></li>
+                                    <li><a href="<?= BASE_URL ?>projects/musicasa/index.php" title="Musicasa">Musicasa</a></li>
+                                    <li><a href="<?= BASE_URL ?>projects/idd/index.php" title="IDD">IDD</a></li>
+                                    <li><a href="<?= BASE_URL ?>projects/map-tools/index.php" title="Map Tools">Map Tools</a></li>
+                            </ul>
                             </div>
-                            <div class="col-lg-12">
-                                <a href="#" class="btn btn-deep-pink btn-small text-extra-small border-radius-4"><i class="fas fa-play-circle icon-very-small margin-5px-right no-margin-left" aria-hidden="true"></i>Purchase Now</a>
-                            </div>
-                            <div class="col-md-12 margin-100px-top text-center">
-                                <div class="icon-social-medium margin-three-bottom">
-                                    <a href="https://www.facebook.com/" target="_blank" class="text-extra-dark-gray text-deep-pink-hover margin-one-lr"><i class="fab fa-facebook-f" aria-hidden="true"></i></a>
-                                    <a href="https://twitter.com/" target="_blank" class="text-extra-dark-gray text-deep-pink-hover margin-one-lr"><i class="fab fa-twitter" aria-hidden="true"></i></a>
-                                    <a href="https://dribbble.com/" target="_blank" class="text-extra-dark-gray text-deep-pink-hover margin-one-lr"><i class="fab fa-dribbble" aria-hidden="true"></i></a>
-                                    <a href="https://plus.google.com" target="_blank" class="text-extra-dark-gray text-deep-pink-hover margin-one-lr"><i class="fab fa-google-plus-g" aria-hidden="true"></i></a>
-                                    <a href="https://www.tumblr.com/" target="_blank" class="text-extra-dark-gray text-deep-pink-hover margin-one-lr"><i class="fab fa-tumblr" aria-hidden="true"></i></a>
-                                </div>
-                            </div>
+
                         </div>
                     </div>
                 </div>
