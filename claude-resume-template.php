987626491b383b9bf1569856e5fa01b3cf6568bc
Michelle M. Muñoz-Dorna <michelle@m3orange.com>
2026-06-18 15:04:37 -0400

    First dynamic JS data

--- claude-resume-template.php
+++ claude-resume-template.php
@@ -13,41 +13,50 @@
 
 
 
-    <section id="resume" class="wow fadeIn">
-        <div class="container">
 
-            <!-- One experience listing ---- Begin -------->
+<section id="resume" class="wow fadeIn">
+    <div class="container">
+        <div id="work-experience-list">
+            <!-- Jobs will be injected here -->
+        </div>
+    </div>
+</section>
 
+    <script>
+    async function loadWorkExperience() {
+        const res = await fetch('/api/work-experience.php');
+        const jobs = await res.json();
+
+        const container = document.getElementById('work-experience-list');
+
+        container.innerHTML = jobs.map(job => `
             <div class="row grid-lines listing-header">
                 <div class="col col-12 col-md-8">
-                    <h5>[company]</h5>
+                    <h5>${job.company}, ${job.job_title}</h5>
                 </div>
                 <div class="col col-12 col-md-4">
-                    [city], [date_start] - [date_end]
+                    ${job.city}, ${job.date_start} – ${job.date_end ?? 'Present'}
                 </div>
             </div>
 
             <div class="row grid-lines rsm-item-header">
                 <div class="col col-12 col-md-12">
                     <div class="rsm-item-intro">
-                    [intro]
+                        ${job.intro ?? ''}
                     </div>
+                    ${job.bullets && job.bullets.length ? `
                     <div class="rsm-item-bullets">
                         <ul>
-                        [bullets]
+                            ${job.bullets.map(b => `<li>${b}</li>`).join('')}
                         </ul>
-                    </div>
+                    </div>` : ''}
                 </div>
             </div>
+        `).join('');
+    }
 
-            <!-- One experience listing ---- End -------->
-
-
-        </div>
-    </section>
-
-
-
+    loadWorkExperience();
+    </script>
 
 </body>
 
