a14def370f8619c18664d4d81c9698ffb2e2ef6f
Michelle M. Muñoz-Dorna <michelle@m3orange.com>
2026-06-20 17:10:52 -0400

    Education added

--- about.php
+++ about.php
@@ -1,5 +1,16 @@
 <?php require_once('config.php') ?>
 
+<?php
+require_once 'db-config.php';
+
+$pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4', DB_USER, DB_PASS);
+
+$stmt = $pdo->query('SELECT * FROM education ORDER BY sort_order ASC');
+$education = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+
+
 
 <?php include ROOT_PATH . 'includes/header.php'; ?>
 
@@ -21,15 +32,71 @@
     </ul>
 </div> -->
 
-    <?php include ROOT_PATH . 'about/inc-education-listings.php'; ?>
 
-    <section id="resume" class="wow fadeIn">
-        <div class="container">
-            <div id="work-experience-list">
-                <!-- Jobs will be injected here -->
-            </div>
+
+<section>
+    <div class="container resume-area-grid">
+        <div class="item">
+                <section id="resume" class="wow fadeIn">
+                    <div class="container">
+                        <div id="work-experience-list">
+                            <!-- Jobs will be injected here -->
+                        </div>
+                    </div>
+                </section>
         </div>
-    </section>
+
+        <div class="item">
+
+        <!-- <section id="resume-2" class="wow fadeIn">
+                <div class="container">
+                    <div id="education-list">
+                    </div>
+                </div>
+            </section> -->
+
+             <section id="resume-2" class="wow fadeIn">
+                <div class="container">
+                    <div id="education-list">
+
+<?php foreach ($education as $edu): ?>
+    <div class="rsm-item-header">
+        <div class="margin-10px-right"><h6><?= $edu['degree'] ?></h6></div>
+        <div class="margin-10px-right"><h6><?= $edu['institution'] ?> / <?= $edu['city'] ?></h6></div>
+        <div>[<?= $edu['date_start'] ?>-<?= $edu['date_end'] ?>]</div>
+
+    </div>
+    <div class="education-details">
+        <?= $edu['details'] ?>
+    </div>
+
+<?php endforeach; ?>
+                            
+
+                        </div><!-- education-list -->
+                    </div>
+                </div>
+            </section>
+
+
+
+
+
+
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
 
     <?php include ROOT_PATH . 'about/inc-unicorn-and-skills.php'; ?>
 
@@ -37,60 +104,7 @@
 
 
 
-    <section class="fullwidth-slider-01">
-        <!-- start section divider -->
-        <section class="section-divider-light">
-            <div class="container">Stakeholder Presentation [Partial]</div>
-        </section>
-        <!-- end section divider -->
-        <div class="container-fluid" style="padding-right: 0px; padding-left: 0px;">
-
-
-            <div class="row">
-
-
-                <div class="col-12 blog-post-content text-center text-md-center"
-                    style="padding-right: 0px; padding-left: 0px;">
-
-                    <div class="swiper-full-screen swiper-cb-fullwidth-screens swiper-container white-move"
-                        data-slider-options='{ 
-                    "loop": true, 
-                    "slidesPerView": "1", 
-                    "allowTouchMove":true, 
-                    "autoplay": false, 
-                    "keyboard": { "enabled": true, "onlyInViewport": true }, 
-                    "navigation": { "nextEl": ".swiper-button-next", 
-                    "prevEl": ".swiper-button-prev" }, 
-                    "pagination": { "el": ".swiper-pagination", "clickable": true } }'>
-
-                        <div class="swiper-wrapper">
-                            <div class="swiper-slide "><img class="cb-screens"
-                                    src="<?= BASE_URL ?>projects/admin/admin-presentation-01.png"></div>
-                            <div class="swiper-slide "><img class="cb-screens"
-                                    src="<?= BASE_URL ?>projects/admin/admin-presentation-02.png"></div>
-                            <div class="swiper-slide "><img class="cb-screens"
-                                    src="<?= BASE_URL ?>projects/admin/admin-presentation-03.png"></div>
-                            <div class="swiper-slide "><img class="cb-screens"
-                                    src="<?= BASE_URL ?>projects/admin/admin-presentation-04.png"></div>
-                            <div class="swiper-slide "><img class="cb-screens"
-                                    src="<?= BASE_URL ?>projects/admin/admin-presentation-05.png"></div>
-                        </div>
-                        <div
-                            class="swiper-pagination swiper-pagination-square swiper-pagination-white swiper-full-screen-pagination">
-                        </div>
-                        <div class="swiper-button-prev swiper-button-black-highlight"></div>
-                        <div class="swiper-button-next swiper-button-black-highlight"></div>
-
-                    </div>
-                </div>
-
-
-            </div>
-        </div>
-    </section>
-
-
-    <script>
+    <!-- <script>
         async function loadEducation() {
             const res = await fetch('/api/education.php');
             const entries = await res.json();
@@ -99,7 +113,7 @@
 
             container.innerHTML = entries.map(e => `
                 <div class="row rsm-item-header">
-                    <div class="col col-12 col-xl-8 col-lg-12 p-0">
+                    <div class="col col-12 p-0">
                         <h6>${e.degree}, ${e.field_of_study} / ${e.institution}, ${e.city} / ${e.date_start}–${e.date_end ?? 'Present'}</h6>
                         ${e.details ? `<div class="education-details">${e.details}</div>` : ''}
                     </div>
@@ -108,7 +122,7 @@
         }
 
         loadEducation();
-    </script>
+    </script> -->
 
 
 
@@ -121,16 +135,16 @@
 
             container.innerHTML = jobs.map(job => `
             <div class="row rsm-item-header">
-                <div class="col col-12 col-xl-8 col-lg-12 p-0">
+                <div class="col col-12 col-xl-8 col-lg-9 col-md-12 p-0">
                     <h6>${job.company}, ${job.job_title}</h6>
                 </div>
-                <div class="col col-12 col-xl-4 col-lg-12 p-0 rsm-city-date">
+                <div class="col col-12 col-xl-4 col-lg-6 col-md-12 p-0 rsm-city-date">
                     ${job.city} <span class="divider">|</span> ${job.date_start} – ${job.date_end ?? 'Present'}
                 </div>
             </div>
 
             <div class="row rsm-item-details">
-                <div class="col col-12 col-xl-9 col-md-12">
+                <div class="col col-12">
                     <div class="rsm-item-intro font-weight-500">
                         ${job.intro ?? ''}
                     </div>
