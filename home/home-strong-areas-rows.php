3901ab04ce6cf97ccb67ad1cef957ec5cca5c5c5
Michelle M. Muñoz-Dorna <michelle@m3orange.com>
2026-07-08 16:50:57 -0400

    Strong areas rows added

---
+++ home/home-strong-areas-rows.php
@@ -0,0 +1,33 @@
+<div class="col col-12 last-paragraph-no-margin">
+
+   <div class="topic-rows">
+      <div class="upper-text-area">
+        <div class="row-left-side">
+          <div class="margin-10px-bottom card-number">[01]</div>
+          <h6 class="font-weight-700 alt-font-2 margin-10px-bottom">Design Thinking</h6>
+        </div>
+      <p>I lean on team workshops and design thinking exercises to break down <i class="font-weight-600">what we understand to be the problem</i>, uncover user pain points and areas of opportunity, and reach alignment on what <i class="font-weight-600">“the real problem”</i> is.</p>
+      </div>
+      <div class="row">
+      <img src="<?= BASE_URL ?>home/usic-artifacts.png" />
+      </div>
+    </div>
+
+    <div class="topic-rows">
+      <div class="row-left-side">
+        <div class="margin-10px-bottom card-number">[01]</div>
+        <h6 class="font-weight-700 alt-font-2 margin-10px-bottom">Deep, Meaningful Research</h6>
+      </div>
+      <p>Turning messy interview data and tangled processes into clear next steps for everyone.</p>
+    </div>
+
+    <div class="topic-rows">
+      <div class="row-left-side">
+        <div class="margin-10px-bottom card-number">[01]</div>
+        <h6 class="font-weight-700 alt-font-2 margin-10px-bottom">AI to Amplify</h6>
+      </div>
+      <p>Leveraging AI to optimize my daily work, by handling the busywork or allowing me to iterate faster. I particularly find it most insightful when synthesizing research and needing to discover relationships and patterns, as well as rapid prototyping to test workflows and validate with engineering.</p>
+    </div>
+
+
+</div>
