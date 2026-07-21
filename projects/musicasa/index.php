a7e01646b5ddada41d873e38f4d58c99b71ee156
Michelle M. Muñoz-Dorna <michelle@m3orange.com>
2026-07-21 19:56:18 -0400

    Video again.

--- projects/musicasa/index.php
+++ projects/musicasa/index.php
@@ -122,19 +122,19 @@ $slideshow = getAssets($pdo, 1, 'slideshow');
                     Your browser does not support the video tag.
                     </video>
                 </div>
-                                <div class="col col-12 video-area">
-                    <!-- No controls, and this order: autoplay loop muted playsinline poster-->
-                    <video autoplay loop muted playsinline poster="video-player-placeholder.png">
-                    <source src="<?= BASE_URL ?>projects/musicasa/assets/260519_1336-musicasa-demo-1000x622.mp4" type="video/mp4">  
-                    Your browser does not support the video tag.
-                    </video>
-                </div>
-<div class="col col-12 video-area">
+<div class="col col-12">
+<!-- No controls, and this order: autoplay loop muted playsinline poster-->
+<video autoplay loop muted playsinline poster="video-player-placeholder.png">
+<source src="<?= BASE_URL ?>projects/musicasa/assets/260519_1336-musicasa-demo-1000x622.mp4" type="video/mp4">  
+Your browser does not support the video tag.
+</video>
+</div>
+<div class="col col-12">
 Previous
-    <video autoplay loop muted playsinline>
-        <source src="<?= BASE_URL ?>projects/musicasa/assets/260519_1336-musicasa-demo-1000x622.mp4" type="video/mp4">  
-        Your browser does not support the video tag.
-    </video>
+<video autoplay loop muted playsinline>
+<source src="<?= BASE_URL ?>projects/musicasa/assets/260519_1336-musicasa-demo-1000x622.mp4" type="video/mp4">  
+Your browser does not support the video tag.
+</video>
 </div>
             </div>
         </div>
