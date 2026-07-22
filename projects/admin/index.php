4d69044024953f38b2f0943d9f3222e677d776bf
Michelle M. Muñoz-Dorna <michelle@m3orange.com>
2026-07-22 09:06:20 -0400

    Mux moreee

--- projects/admin/index.php
+++ projects/admin/index.php
@@ -60,21 +60,6 @@ $slideshow = getAssets($pdo, 3, 'slideshow');
         ></mux-player>
         </div>
         </div>
-        <div class="row">
-        <div class="col-12 col-md-6 offset-md-3">
-
-        <iframe
-        loop
-        muted
-        autoplay
-        src="https://player.mux.com/EcHgOK9coz5K4rjSwOkoE7Y7O01201YMIC200RI6lNxnhs?metadata-video-title=Test%20VOD&metadata-viewer-user-id=user-id-007"
-        allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
-        allowfullscreen="true"
-        ></iframe>
-        </div>
-
-
-        </div>
     </div>
 </section>
 
