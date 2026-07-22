50683fd4aca0a81d1af9301d0d53750e710cc744
Michelle M. Muñoz-Dorna <michelle@m3orange.com>
2026-07-22 09:37:21 -0400

    Adding YouTube

--- projects/admin/index.php
+++ projects/admin/index.php
@@ -42,6 +42,8 @@ $slideshow = getAssets($pdo, 3, 'slideshow');
     <?php include ROOT_PATH . 'projects/description-intro-global.php'; ?>
     <!-- end product information section -->
 
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/G5EvbNWh1-k?si=WNt7JQbiWQkTJJ3w&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
 <section>
     <div class="container">
         <div class="row">
